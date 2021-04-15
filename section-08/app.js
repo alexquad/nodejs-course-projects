bb5d9beb43381dcfa239e3716a47376a3ac31046
Alexandre <alexandre.dqmf@gmail.com>
2021-04-15 12:57:39 -0300

    starting section 08

---
+++ section-08/app.js
@@ -0,0 +1,13 @@
+// core modules
+const path = require('path');
+// tp modules
+const express = require('express');
+
+// init
+const app = express();
+app.set('view engine', 'ejs');
+
+// import routes
+
+// run
+app.listen(3000);
